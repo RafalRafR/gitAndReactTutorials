afff0592370bcf70aefcfe7180c356c803c71b68
rafkruszewski <kruszewski.rafal@gmail.com>
2017-12-02 20:15:22 +0000

    initial environment setup

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+require('react');
+
+class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div className="exampleContent">
+                <h1>Header example.</h1>
+            </div>
+        );
+    }
+}
+
+module.exports('ExampleComponent');
